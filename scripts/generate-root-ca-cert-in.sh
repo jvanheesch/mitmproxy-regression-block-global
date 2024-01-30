9cb77466a1f8fd3c6c06b16f32f26eeb1de036c8
jvanheesch <jvanheesch@gmail.com>
2024-01-30 15:23:08 +0100

    add script to generate gateway-in's root CA cert (used to sign CSRs)

---
+++ scripts/generate-root-ca-cert-in.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Generating root CA certificate"
+MSYS_NO_PATHCONV=1 docker run --rm -v $PWD/../ca/in:/ca alpine/openssl genrsa -out /ca/ca.key 4096
+MSYS_NO_PATHCONV=1 docker run --rm -v $PWD/../ca/in:/ca alpine/openssl req -new -x509 -days 3650 -key /ca/ca.key -out /ca/ca.pem -config /ca/ca_config.cnf
