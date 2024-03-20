f6abe67a577b2e6a585df8e7c0e643c414a3d4f7
jvanheesch <jvanheesch@gmail.com>
2024-03-20 12:58:46 +0100

    make generate-root-ca-cert-in.sh generate mitmproxy-ca.pem

--- scripts/generate-root-ca-cert-in.sh
+++ scripts/generate-root-ca-cert-in.sh
@@ -2,3 +2,4 @@
 echo "Generating root CA certificate"
 MSYS_NO_PATHCONV=1 docker run --rm -v $PWD/../ca/in:/ca alpine/openssl genrsa -out /ca/ca.key 4096
 MSYS_NO_PATHCONV=1 docker run --rm -v $PWD/../ca/in:/ca alpine/openssl req -new -x509 -days 3650 -key /ca/ca.key -out /ca/ca.pem -config /ca/ca_config.cnf
+cat ../ca/in/ca.key ../ca/in/ca.pem > ../ca/in/mitmproxy-ca.pem
