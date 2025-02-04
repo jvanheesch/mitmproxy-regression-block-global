406ad376af2ff90bc9bcc393fdea4882414a714d
jvanheesch <jvanheesch@gmail.com>
2025-02-04 18:30:57 +0100

    make chrome ignore ssl issues for certs generated by root ca

--- chrome.sh
+++ chrome.sh
@@ -2,10 +2,12 @@ if [[ "$OSTYPE" == "msys" ]]; then
   start chrome \
   --user-data-dir="$(pwd)/.chrome_profile" \
   --host-resolver-rules="MAP * 127.0.0.1" \
+  --ignore-certificate-errors-spki-list="$(cat ./ca/in/sha256_base64)"
 
 else
   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
   --user-data-dir="./.chrome_profile" \
   --host-resolver-rules="MAP * 127.0.0.1" \
+  --ignore-certificate-errors-spki-list="$(cat ./ca/in/sha256_base64)"
 
 fi
